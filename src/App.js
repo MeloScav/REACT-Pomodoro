370036218c74a92536f8fe63e81f7ef6c4a52625
Méloé <meloscavezzoni@gmail.com>
2020-04-09 12:03:58 +0200

    incrementation + decrementation + reset Timer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React, { useState, useEffect } from "react";
 import Buttons from "./components/buttons";
 
 const App = () => {
-  const [seconds, setSecondes] = useState(60 * 25);
+  const [seconds, setSeconds] = useState(60 * 25);
+
+  // INCREMENTATION
+  const incrementSeconds = () => {
+    setSeconds(seconds + 60);
+  };
+
+  // DECREMENTATION
+  const decrementSeconds = () => {
+    setSeconds(seconds - 60);
+  };
+
+  // RESET
+  const resetTimer = () => {
+    setSeconds(60 * 25);
+  };
 
   return (
     <div>
@@ -11,7 +26,17 @@ const App = () => {
         .padStart(2, "0")}:${`${seconds % 60}`
         .toString()
         .padStart(2, "0")}`}</h1>
-      <Buttons />
+      <Buttons
+        addOnClick={() => {
+          incrementSeconds();
+        }}
+        substractionOnClick={() => {
+          decrementSeconds();
+        }}
+        resetOnClick={() => {
+          resetTimer();
+        }}
+      />
     </div>
   );
 };
