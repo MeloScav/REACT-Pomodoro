c80da924ba7c3c0db71fc7c39b891051274d2baa
Méloé <meloscavezzoni@gmail.com>
2020-04-21 14:46:27 +0200

    Add a arrow svg in a timer component

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import DownArrowSVG from "./SVG/down-arrow-svg";
+import UpArrowSVG from "./SVG/up-arrow-svg";
 
 const Timer = (props) => {
   return (
     <div className={"timer"}>
       <button type="button" onClick={props.addOnClick}>
-        {"+"}
+        <UpArrowSVG />
       </button>
       <h2>{`${Math.floor(props.second / 60)
         .toString()
@@ -12,7 +14,7 @@ const Timer = (props) => {
         .toString()
         .padStart(2, "0")}`}</h2>
       <button type="button" onClick={props.substractionOnClick}>
-        {"-"}
+        <DownArrowSVG />
       </button>
     </div>
   );
